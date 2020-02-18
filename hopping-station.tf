4b16aeed4a2264ceb30037a5579dcc411603ded8
Yaroslav <yorimark@abv.bg>
2020-02-18 13:32:24 +0200

    First Upload

---
+++ hopping-station.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "hopping-station" {
+
+  ami                    = "ami-0be057a22c63962cb"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.ptfe-vpc-public-subnet.id         
+  vpc_security_group_ids = [aws_security_group.vpc_ssh_icmp_echo_sg.id] 
+  key_name               = aws_key_pair.yaro-ssh.id                     
+  tags = {
+    Name = "Yaro-ptfe-hopping-station"
+  }
+
+
+
+  depends_on = [aws_internet_gateway.ptfe-vpc-internet-gw]
+}
+
+output "hopping-station" {
+  value = aws_instance.hopping-station.public_ip
+}
